f4b5d15a7a6c80d66c7e3305fdfaee2a9dc4cd3f
iOS Dev <emag.ios.test@gmail.com>
2022-03-16 11:39:15 +0200

    fixed SPM

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ZSWTaggedString",
-            targets: ["ZSWTaggedString"]),
+            targets: ["ZSWTaggedStringObjC", "ZSWTaggedStringSwift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,15 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ZSWTaggedString",
+            name: "ZSWTaggedStringObjC",
             dependencies: [],
-            path: "ZSWTaggedString/Classes"),
+            path: "ZSWTaggedString/Classes/ObjC"),
+        .target(
+            name: "ZSWTaggedStringSwift",
+            dependencies: [],
+            path: "ZSWTaggedString/Classes/Swift"),
         .testTarget(
             name: "ZSWTaggedStringTests",
-            dependencies: ["ZSWTaggedString"]),
+            dependencies: []),
     ]
 )
